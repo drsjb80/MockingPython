6598889b2e5a3bab49b58f99b499a65a3d9a7fb1
drsjb80 <drsjb80@gmail.com>
2020-03-17 15:03:33 -0600

    initial

---
+++ thecastleargv.py
@@ -0,0 +1,16 @@
+# https://docs.python.org/3.3/library/argparse.html
+
+import argparse
+import sys
+import unittest
+
+class TestArgs(unittest.TestCase):
+    def test_help(self):
+        sys.argv = ["thecastleargv", "--foo"]
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--foo', action='store_true')
+        args = parser.parse_args()
+        print(args)
+        self.assertTrue('foo' in args)
+        self.assertFalse('bar' in args)
+        self.assertEqual(args.foo, True)
